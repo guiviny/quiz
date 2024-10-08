d9eedc65289982eaaac12d257dd46a11b324bf13
Guilherme <guigui.vinyy@gmail.com>
2024-10-08 00:10:34 -0300

    Caracteres

--- questions.js
+++ questions.js
@@ -14,63 +14,63 @@ let questions = [
         //questão 1
         question: '2 - Escolha a opção que descreve o nome e imagem que representa o Mascote da linguagem JAVA:',
         options: [
-            'a',
-            'b' 
+            'a)',
+            'b)' 
         ],
         answer: 2
     },
     {
         //questão 2
-        question: 'Suponhamos que linguagem "X" não possua suporte a classes abstratas. Como podemos simular esse comportamento?',
+        question: '3 - Suponhamos que linguagem "X" não possua suporte a classes abstratas. Como podemos simular esse comportamento?',
         options: [
-            'a',
-            'b' 
+            'a)',
+            'b)' 
         ],
         answer: 1
     },
     {
         //questão 3
-        question: 'Quais dos dois códigos a seguir utilizam o conceito de encapsulamento da Programação Orientada a Objetos (POO)?',
+        question: '4 - Quais dos dois códigos a seguir utilizam o conceito de encapsulamento da Programação Orientada a Objetos (POO)?',
         options: [
-            'a',
-            'b' 
+            'a)',
+            'b)' 
         ],
         answer: 1
     },
 
     {
         //questão 4
-        question: 'Quais dos códigos a seguir apresentam corretamente o uso de sobrecarga de métodos da linguagem Java?',
+        question: '5 - Quais dos códigos a seguir apresentam corretamente o uso de sobrecarga de métodos da linguagem Java?',
         options: [
-            'a',
-            'b' 
+            'a)',
+            'b)' 
         ],
         answer: 1
     },
 
     {
         //questão 5
-        question: 'Qual é o boilerplate de uma classe generalista?',
+        question: '6 - Qual é o boilerplate de uma classe generalista?',
         options: [
-            'a',
-            'b' 
+            'a)',
+            'b)' 
         ],
         answer: 2
     },
 
     {
         //questão 6
-        question: 'Selecione abaixo a imagem que representa o primeiro protótipo criado pela Sun Microsystems, considerado um dos primeiros assistentes pessoais digitais:',
+        question: '7 - Selecione abaixo a imagem que representa o primeiro protótipo criado pela Sun Microsystems, considerado um dos primeiros assistentes pessoais digitais:',
         options: [
-            'a',
-            'b' 
+            'a)',
+            'b)' 
         ],
         answer: 2
     },
 
     {
         //questão 7
-        question: 'Quem foi o criador do Python?',
+        question: '8 - Quem foi o criador do Python?',
         options: [
             'a) Guido Van Rossum',
             'b) Brendan Eich' 
@@ -80,9 +80,9 @@ let questions = [
        
     {
         //questão 8
-        question: 'Qual dessa linguagem é Orientada a Objeto?',
+        question: '9 - Qual dessa linguagem é Orientada a Objeto?',
         options: [
-            'a) ',
+            'a)',
             'b)' 
         ],
         answer: 2
